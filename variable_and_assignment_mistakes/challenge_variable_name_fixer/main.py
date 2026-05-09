11343c70584834b3dd8521ab391d7ed65d6683b2
BotLife88 <>
2026-05-09 18:50:23 +0000

    Challenge: Variable Name Fixer

---
+++ variable_and_assignment_mistakes/challenge_variable_name_fixer/main.py
@@ -0,0 +1,8 @@
+def fix_variable_names():
+    first_value = 10
+    my_value = 5
+    actual_value = 7
+    total = first_value + my_value + actual_value
+    print("Total:", total)
+
+fix_variable_names()
